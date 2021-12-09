8a3f18425bda012378dfb885ea5ba2800bad2dcf
banuteja <93367938+banuteja123@users.noreply.github.com>
2021-12-09 09:12:38 +0000

    add a new change

---
+++ fibonacinumber.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+int main () {
+    int n,i,f,s,p;
+    printf("the number of terms:");
+    scanf ("%d",&n);
+    f=0;
+    s=1;
+   printf("%d",f);
+    printf("%d",s);
+    for  (i=4;i<n;i++)
+    {
+     p=f+s ;
+     printf("%d",p);
+     f=s;
+     s=p;
+    }
+ return 0;
+ 
+}
