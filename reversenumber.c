98e14a283e07003aac8f58e27bf80e82110af7b0
banuteja <93367938+banuteja123@users.noreply.github.com>
2021-12-14 09:23:12 +0000

    add a new change

---
+++ reversenumber.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()   {
+  int num,X,Y=0;
+  printf ("enter a number :");
+  scanf("%d",&num);
+while(num!=0)
+{
+  X=num%10;
+  Y=Y*10+X;
+  num=num/10;
+}
+printf("the reverse of a number=%d",Y);
+}
