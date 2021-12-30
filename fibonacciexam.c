6fa44957ad57de3828d6d0ad194e390b6cbf06b3
banuteja <93367938+banuteja123@users.noreply.github.com>
2021-12-30 18:09:19 +0000

    add a new change

--- fibonacciexam.c
+++ fibonacciexam.c
@@ -1,4 +1,12 @@
-# include <stdio.h>
+/* algorithm
+Step 1- Start
+Step 2- enter how many numbers required
+Step 3-put n1=0, n2=1
+Step 4-Print First two terms of series
+Step 5-use for loop n3=n1+n2;n1=n2;n2=n3;
+step 6-stop*/
+
+#include <stdio.h>
 int main () {
     int n,i,f,s,p;
     printf("the number of terms:");
@@ -17,3 +25,5 @@ int main () {
  return 0;
  
 }
+
+
