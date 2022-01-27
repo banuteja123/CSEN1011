64450965f794e907e35f0a8a94ce06d34368518b
banuteja <93367938+banuteja123@users.noreply.github.com>
2022-01-27 10:15:02 +0000

    add a new change

---
+++ someprogram.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define LENGTH 10
+int main ()  {
+ char string_01[length];
+ printf("Enter atleast 10 characters for the string:  ");
+ fgets(string_01);
+ printf("The string entered is: ");
+ puts(string_01);
+ return 0;
+}
